d3889e4235b816cf40a067832d7c48bb8d3d898f
Nazar <krutosem2345@gmail.com>
2020-01-28 21:16:54 +0200

    Lab 2_1 (v.3)

--- Lab2/Lab2_1/src/com/company/Main.java
+++ Lab2/Lab2_1/src/com/company/Main.java
@@ -4,40 +4,46 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	    int x, y, z;
+	    int x, y;
+	    boolean CheckValue = false;
         Scanner in = new Scanner(System.in);
 
-	    System.out.print("x = ");
-	    x=in.nextInt();
-        System.out.print("y = ");
-        y=in.nextInt();
-        
-        if((x==0)&&(y==0)){
-            System.out.print("2\t");
-        }
-        else if((x==0)&&(y==1)){
-            System.out.print("2\t");
-        }
-        else if((x==0)&&(y==2)){
-            System.out.print("0\n");
-        }
-        else if((x==1)&&(y==0)){
-            System.out.print("1\t");
-        }
-        else if((x==1)&&(y==1)){
-            System.out.print("2\t");
-        }
-        else if((x==1)&&(y==2)){
-            System.out.print("0\n");
-        }
-        else if((x==2)&&(y==0)){
-            System.out.print("2\t");
-        }
-        else if((x==2)&&(y==1)){
-            System.out.print("2\t");
-        }
-        else if((x==2)&&(y==2)) {
-            System.out.print("1\n");
+        while (CheckValue == false) {
+
+            System.out.print("x = ");
+            x = in.nextInt();
+            System.out.print("y = ");
+            y = in.nextInt();
+
+
+            if ((x == 0) && (y == 0)) {
+                System.out.print("2");
+                CheckValue = true;
+            } else if ((x == 0) && (y == 1)) {
+                System.out.print("2");
+                CheckValue = true;
+            } else if ((x == 0) && (y == 2)) {
+                System.out.print("0");
+                CheckValue = true;
+            } else if ((x == 1) && (y == 0)) {
+                System.out.print("1");
+                CheckValue = true;
+            } else if ((x == 1) && (y == 1)) {
+                System.out.print("2");
+                CheckValue = true;
+            } else if ((x == 1) && (y == 2)) {
+                System.out.print("0");
+                CheckValue = true;
+            } else if ((x == 2) && (y == 0)) {
+                System.out.print("2");
+                CheckValue = true;
+            } else if ((x == 2) && (y == 1)) {
+                System.out.print("2");
+                CheckValue = true;
+            } else if ((x == 2) && (y == 2)) {
+                System.out.print("1");
+                CheckValue = true;
+            } else System.out.println("Invalid values!");
         }
     }
 }
